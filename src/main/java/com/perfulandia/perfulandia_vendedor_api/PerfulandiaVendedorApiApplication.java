3203635a197504c2ae99528ed002bff77e30cf61
Andru Alarcon <idazhdz@gmail.com>
2025-05-27 22:39:59 -0400

    Commit inicial del proyecto

---
+++ src/main/java/com/perfulandia/perfulandia_vendedor_api/PerfulandiaVendedorApiApplication.java
@@ -0,0 +1,13 @@
+package com.perfulandia.perfulandia_vendedor_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfulandiaVendedorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfulandiaVendedorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perfulandia/perfulandia_vendedor_api/PerfulandiaVendedorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandia.perfulandia_vendedor_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfulandiaVendedorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
