b417a37934dcc9da8c98320ac197f88e81cdff31
Masek830 <victor.ramos.saldias@gmail.com>
2025-07-03 22:11:02 -0400

    Hateoas

--- src/main/java/com/perfulandia/perfulandia_vendedor_api/dto/VendedorDTO.java
+++ src/main/java/com/perfulandia/perfulandia_vendedor_api/dto/VendedorDTO.java
@@ -1,12 +1,22 @@
 package com.perfulandia.perfulandia_vendedor_api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.springframework.hateoas.RepresentationModel;
 
 @Data
-public class VendedorDTO {
+@AllArgsConstructor
+@NoArgsConstructor
+public class VendedorDTO extends RepresentationModel<VendedorDTO> {
     private Integer id_vendedor;
     private Integer id_usuario;
     private String nombre_completo;
     private String rut;
     private String area_ventas;
+public Integer getId() {
+    return this.id_vendedor;
 }
+}
+
